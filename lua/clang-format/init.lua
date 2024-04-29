752367e8cdb5c9d1c1ab099fe2be48c7bb768d82
Kedar <56258764+ked1108@users.noreply.github.com>
2024-04-29 12:24:37 +0530

    added cpp and hpp

--- lua/clang-format/init.lua
+++ lua/clang-format/init.lua
@@ -7,7 +7,7 @@ function M.setup(opts)
 	assert(opts.clangFormatPath, "Expected .clang-format path to be specified. Set clangFormatPath in opts.")
 
 	vim.api.nvim_create_autocmd({"BufWritePost"}, {
-		pattern = {"*.c", "*.h"},
+		pattern = {"*.c", "*.h", "*.cpp", ".hpp"},
 		callback = function(ev)
 			local out = vim.fn.system({"clang-format", "-i", ev.match, "--style=file:" .. opts.clangFormatPath})
 			print(out)
